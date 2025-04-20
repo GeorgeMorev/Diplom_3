8290e6c6d8d8ae50565b15d9fd66dcfa63627e9a
George Morev <morevgeorge@gmail.com>
2025-04-20 14:58:46 +0300

    Добавлен класс URLs с ссылками на стрницы приложения

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+class URLs:
+    BASE_URL = "https://stellarburgers.nomoreparties.site"
+
+    MAIN_PAGE = f"{BASE_URL}/"
+    LOGIN_PAGE = f"{BASE_URL}/login"
+    REGISTER_PAGE = f"{BASE_URL}/register"
+    FEED_PAGE = f"{BASE_URL}/feed"
+    PROFILE_PAGE = f"{BASE_URL}/account/profile"
+    ORDER_HISTORY_PAGE = f"{BASE_URL}/account/order-history"
+    RECOVERY_PASSWORD_PAGE = f"{BASE_URL}/forgot-password"
