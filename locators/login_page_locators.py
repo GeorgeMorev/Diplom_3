232906207135b441554894f2fae2d76da95f723a
George Morev <morevgeorge@gmail.com>
2025-04-28 13:51:06 +0300

    Класс LoginPageLocators изменен

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -4,5 +4,6 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators:
     EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/following-sibling::input")
     PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']/following-sibling::input")
-    ENTER_BUTTON = (By.XPATH, "//a[text()='Войти']")
-    REGISTER_BUTTON = (By.XPATH, "//a[text()='Зарегистрироваться']")
+    ENTER_BUTTON = (By.XPATH, "//button[contains(@class, 'button_button_type_primary') and text()='Войти']")
+    REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
+    RECOVERY_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
