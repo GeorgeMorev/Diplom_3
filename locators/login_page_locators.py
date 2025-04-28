f3f5b6897577b565de5aab3d27f6b93ed590372c
George Morev <morevgeorge@gmail.com>
2025-04-28 15:05:34 +0300

    Добавлены файл и класс RecoveryPasswordPage

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -2,8 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/following-sibling::input")
-    PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']/following-sibling::input")
+    LOGIN_EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/following-sibling::input")
+    LOGIN_PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']/following-sibling::input")
     ENTER_BUTTON = (By.XPATH, "//button[contains(@class, 'button_button_type_primary') and text()='Войти']")
-    REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
-    RECOVERY_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
+    REGISTER_LINK = (By.XPATH, "//a[@href='/register']")
+    RECOVERY_PASSWORD_LINK = (By.XPATH, "//a[@href='/forgot-password']")
