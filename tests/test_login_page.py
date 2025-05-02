aab4a509f62143b1f25f6586624612ae5f7d20bd
George Morev <morevgeorge@gmail.com>
2025-05-02 07:29:17 +0300

    Добавлен класс TestLoginPage

---
+++ tests/test_login_page.py
@@ -0,0 +1,14 @@
+import pytest
+from locators.login_page_locators import LoginPageLocators
+from pages.login_page import LoginPage
+from config.urls import URLs
+
+
+class TestLoginPage:
+    main_page_url = URLs.MAIN_PAGE
+
+    def test_go_to_constructor(self, driver):
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.click_constructor_button()
+        assert driver.current_url == self.main_page_url
